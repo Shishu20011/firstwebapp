987277b6ad3c1bcb8f0cdaf81648c6319efe23b2
student Shishupal-singh.in <Shishupalrajput2001@gmail.in>
2025-03-23 11:28:33 +0530

    PrimeInRange

---
+++ PrimeInRange.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class PrimeInRange {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter starting number: ");
+        int start = scanner.nextInt();
+
+        System.out.print("Enter ending number: ");
+        int end = scanner.nextInt();
+
+        System.out.println("Prime numbers between " + start + " and " + end + " are:");
+
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n % i == 0) return false;
+        }
+
+        return true;
+    }
+}
